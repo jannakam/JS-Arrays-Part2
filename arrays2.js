a8cafaac74e08a6cb4350b565f41e8859d572813
jannakam <jannakalmuqaisib@gmail.com>
2024-10-08 09:45:08 +0300

    JS Arrays Task 2 completed

---
+++ arrays2.js
@@ -0,0 +1,154 @@
+// TASK 1
+
+/**
+ * isArrayLengthOdd(numbers):
+ * - receives array `numbers`
+ * - returns true if array has an odd number of elements
+ * - returns false otherwise
+ *
+ * e.g.
+ * isArrayLengthOdd([1, 2, 3]) -> true
+ * isArrayLengthOdd([1, 2, 3, 4]) -> flase
+ */
+function isArrayLengthOdd(numbers) {
+  // Your code here
+  arrayLength = numbers.length;
+  if (arrayLength % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// To check our work
+console.log(isArrayLengthOdd([1, 2, 3]));
+console.log(isArrayLengthOdd([1, 2, 3, 4]));
+
+// TASK 2
+
+/**
+ * isArrayLengthEven(numbers):
+ * - receives array `numbers`
+ * - returns true if array has an even number of elements
+ * - returns false otherwise
+ *
+ * e.g.
+ * isArrayLengthEven([1, 2, 3]) -> false
+ * isArrayLengthEven([1, 2, 3, 4]) -> true
+ */
+function isArrayLengthEven(numbers) {
+  // Your code here
+  arrayLength = numbers.length;
+  if (arrayLength % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// To check our work
+console.log(isArrayLengthEven([1, 2, 3]));
+console.log(isArrayLengthEven([1, 2, 3, 4]));
+
+// TASK 3
+
+/**
+ * addLailaToArray(instructors):
+ * - receives array `instructors`
+ * - returns a new array that's a copy of array `instructors` with additional string "Laila"
+ *
+ * e.g.
+ * addLailaToArray(["Mshary", "Hasan"]) -> ["Mshary", "Hasan", "Laila"]
+ */
+function addLailaToArray(instructors) {
+  // Your code here
+  instructors.push("Laila");
+  return instructors;
+}
+
+// To check our work
+console.log(addLailaToArray(["Mshary", "Hasan"]));
+
+// TASK 4
+
+/**
+ * eliminateTeam(teams):
+ * - receives array `teams`
+ * - removes the last element from the array and return it
+ *
+ * e.g.
+ * eliminateTeam(["Brazil", "Germany", "Italy"]) -> "Italy"
+ */
+function eliminateTeam(teams) {
+  // Your code here
+  return teams.pop();
+}
+
+// To check our work
+console.log(eliminateTeam(["Brazil", "Germany", "Italy"]));
+
+// CHALLENGE 1
+
+/**
+ * secondHalfOfArrayIfItIsEven(fruits):
+ * - receives array `fruits`
+ * - returns a new array that's the second half of the original array if it has an even number of elements
+ * - returns an empty array if it has an odd number of elements
+ *
+ * e.g.
+ * secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi"]) -> ["banana", "kiwi"]
+ * secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi", "blueberry"]) -> []
+ */
+function secondHalfOfArrayIfItIsEven(fruits) {
+  // Your code here
+  arrayLength = fruits.length;
+  if (arrayLength % 2 === 0) {
+    return fruits.slice(arrayLength / 2, arrayLength);
+  } else {
+    return [];
+  }
+}
+
+// To check our work
+console.log(secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi"]));
+console.log(
+  secondHalfOfArrayIfItIsEven([
+    "apple",
+    "orange",
+    "banana",
+    "kiwi",
+    "blueberry",
+  ])
+);
+
+// CHALLENGE 2
+
+/**
+ * youGottaCalmDown(shout):
+ * - receives a string `shout`
+ * - returns the string `shout` with at most one exclamation mark (!) at the end.
+ *
+ * e.g.
+ * youGottaCalmDown("HI!!!!!!!!!!") -> "HI!"
+ * youGottaCalmDown("Taylor Schwift!!!!!!!!!!!") -> "Taylor Schwift!"
+ * youGottaCalmDown("Hellooooo") -> "Hellooooo"
+ *
+ * Hint:
+ * - Use number method .indexOf()
+ * - Use string method .slice()
+ */
+function youGottaCalmDown(shout) {
+  // Your code here
+  lengthOfString = shout.length;
+  exclamationIndex = shout.indexOf("!");
+  if (exclamationIndex === -1) {
+    return shout;
+  } else {
+    return shout.slice(0, exclamationIndex + 1);
+  }
+}
+
+// To check our work
+console.log(youGottaCalmDown("HI"));
+console.log(youGottaCalmDown("HI!"));
+console.log(youGottaCalmDown("HI!!!!!!"));
